1a016e20f83c1193c0495382c7c40c91920042f4
kn1kn1 <kn1kn1@users.noreply.github.com>
2017-02-19 15:38:48 +0900

    Add git reset command

--- lint-locally.sh
+++ lint-locally.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git submodule init
 git submodule update
-git submodule foreach 'git checkout master -- .; git pull origin master'
+git submodule foreach 'git reset --hard HEAD; git checkout master -- .; git pull origin master'
 
 ls -al
 
