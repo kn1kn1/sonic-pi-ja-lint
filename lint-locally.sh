751a68e7f83fd3039025fd7faf9832748bb9081a
kn1kn1 <kn1kn1@users.noreply.github.com>
2018-01-12 08:36:00 +0900

    :bug: Fix the error - i18n-tool.rb not found

--- lint-locally.sh
+++ lint-locally.sh
@@ -5,7 +5,7 @@ ls -al
 curl https://hosted.weblate.org/download/sonic-pi/tutorial/ja/ -o sonic-pi/etc/doc/lang/sonic-pi-tutorial-ja.po
 
 # generate transrated md files
-sonic-pi/app/server/bin/i18n-tool.rb -t
+sonic-pi/app/server/ruby/bin/i18n-tool.rb -t
 
 # text-lint
 #node_modules/.bin/textlint -f junit -o report.xml sonic-pi/etc/doc/generated/ja/tutorial
